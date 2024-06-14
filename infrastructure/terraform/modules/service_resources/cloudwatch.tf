531043d4ef258a533925677c081cac5ac7010f65
Pietro Ribeiro <Pietro.Ribeiro@stark.dk>
2024-06-14 10:23:14 +0100

    adding log stream for ecs api

--- infrastructure/terraform/modules/service_resources/cloudwatch.tf
+++ infrastructure/terraform/modules/service_resources/cloudwatch.tf
@@ -1,3 +1,8 @@
 resource "aws_cloudwatch_log_group" "ecs_log_group" {
   name = "ecs-omentejovem"
 }
+
+resource "aws_cloudwatch_log_stream" "ecs_log_group_stream" {
+  name           = "awslogs-omentejovem-api"
+  log_group_name = aws_cloudwatch_log_group.ecs_log_group.name
+}
