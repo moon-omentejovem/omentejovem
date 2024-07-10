51218bdc2671e73e5c3d7873d5974e8934dcd720
rib-pietro <ribpietro@gmail.com>
2024-07-10 18:51:13 +0100

    bora

--- client/src/utils/lazyLoading.ts
+++ client/src/utils/lazyLoading.ts
@@ -1,4 +1,4 @@
-import { SyntheticEvent } from "react";
+import { SyntheticEvent } from 'react';
 
 export function addLoadedClass(loadedEvent: SyntheticEvent<HTMLImageElement, Event>) {
   loadedEvent.currentTarget.classList.add('loaded')
