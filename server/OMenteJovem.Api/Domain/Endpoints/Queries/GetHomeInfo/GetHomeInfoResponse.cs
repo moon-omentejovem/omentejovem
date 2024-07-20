cc6960206f4603a13d6eb3a2edbe29fe1124f9d9
rib-pietro <ribpietro@gmail.com>
2024-07-20 18:17:23 +0100

    implementing api for home page

---
+++ server/OMenteJovem.Api/Domain/Endpoints/Queries/GetHomeInfo/GetHomeInfoResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Endpoints.Queries.GetHomeInfo;
+
+public record GetHomeInfoResponse(
+    string Title,
+    string Subtitle,
+    IEnumerable<GetHomeInfoNftResponse> Nfts
+);
+
+public record GetHomeInfoNftResponse(
+    string Title,
+    DateTime? CreatedAt,
+    string ImageUrl
+);
