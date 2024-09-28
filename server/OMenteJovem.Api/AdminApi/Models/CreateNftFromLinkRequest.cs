8ee3f111b04ff1c03acc82a7b118d4def8649e3e
rib-pietro <ribpietro@gmail.com>
2024-09-28 22:05:57 +0100

    creating add from link functionality

---
+++ server/OMenteJovem.Api/AdminApi/Models/CreateNftFromLinkRequest.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models.Enums;
+
+namespace AdminApi.Models;
+
+public class CreateNftFromLinkRequest
+{
+    public string ContractAddress { get; set; } = string.Empty;
+    public string NftId { get; set; } = string.Empty;
+    public NftChain NftChain { get; set; }
+}
