2216ec5ccd47b1652663564b3bf78c75a076e9f4
yungwknd <yungwkndllc@gmail.com>
2024-10-29 20:55:20 -0700

    update api
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,10 +1,4 @@
-const apiKey =
-  process.env.SIMPLEHASH_API_KEY ||
-  'yungwkndllc_sk_2ortb02wp7sp0364r9uf1b88m3p8qr8p'
-const baseUrl =
-  process.env.API_BASE_URL || 'https://api.simplehash.com/api/v0/nfts/assets'
-
 export const api = {
-  baseURL: baseUrl,
-  apiKey: apiKey
+  baseURL: 'https://api.simplehash.com/api/v0/nfts/assets',
+  apiKey: 'yungwkndllc_sk_2ortb02wp7sp0364r9uf1b88m3p8qr8p'
 }
