a4be6ff7b18edb3e3356faf601ec2309c5c63ac8
yungwknd <yungwkndllc@gmail.com>
2024-11-01 10:48:14 -0700

    collections
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- client/src/api/requests/fetchCollections.ts
+++ client/src/api/requests/fetchCollections.ts
@@ -1,15 +1,57 @@
 'use server'
 
 import { api } from '../client'
-import { CollectionsResponse } from '../resolver/types'
+import { NFT } from '../resolver/types'
+import fetch from 'node-fetch'
+
+const ALL_NFTS = [
+  '0x826b11a95a9393e8a3cc0c2a7dfc9accb4ff4e43:5',
+  '0x826b11a95a9393e8a3cc0c2a7dfc9accb4ff4e43:6',
+  '0x826b11a95a9393e8a3cc0c2a7dfc9accb4ff4e43:4',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:1',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:2',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:3',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:4',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:5',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:6',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:7',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:8',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:9',
+  '0x2b3bbde45422d65ab3fb5cdc5427944db0729b50:10'
+]
 
 export async function fetchCollections() {
-  const data = await fetch(`${api.baseURL}/collections`, {
-    ...api,
+  let ALL_DATA: { nfts: NFT[] } = { nfts: [] }
+
+  const formattedQuery = ALL_NFTS.map((nft) => {
+    if (nft.startsWith('KT')) {
+      return ''
+    }
+    const tokenAddress = nft.split(':')[0]
+    const tokenId = nft.split(':')[1]
+    return `ethereum.${tokenAddress}.${tokenId}`
+  })
+    .filter((nft) => nft !== '')
+    .join(',')
+
+  const data = await fetch(`${api.baseURL}?nft_ids=${formattedQuery}`, {
     method: 'GET',
-    next: { revalidate: 600 }
+    headers: {
+      'X-API-KEY': api.apiKey || '',
+      Accept: 'application/json'
+    }
   })
 
   const jsonData = await data.json()
-  return jsonData as CollectionsResponse
+  ALL_DATA = jsonData as { nfts: NFT[] }
+
+  // Order by created_date newest first
+  ALL_DATA.nfts.sort((a, b) => {
+    return (
+      new Date(b.created_date || '').getTime() -
+      new Date(a.created_date || '').getTime()
+    )
+  })
+
+  return ALL_DATA
 }
