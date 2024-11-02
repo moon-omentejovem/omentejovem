afcdb448ca708aad53961ac84810620993d0f60c
yungwknd <yungwkndllc@gmail.com>
2024-11-02 12:18:51 -0700

    force it
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,10 @@
 import { fetchHomeInfo } from '@/api/requests/fetchHomeInfo'
 import HomeContent from './home/content'
 
+// Disable caching - fetch fresh data on every request
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default async function Home() {
   const data = await fetchHomeInfo()
 
