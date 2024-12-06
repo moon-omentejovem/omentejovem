8d1e9e787e395a692786f314503432d65800858c
yungwknd <yungwkndllc@gmail.com>
2024-12-06 14:42:56 -0800

    updates
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- client/src/components/ArtDetails.tsx
+++ client/src/components/ArtDetails.tsx
@@ -11,15 +11,15 @@ interface ArtDetails {
 
 export function ArtDetails({ image, detailedImage, name }: ArtDetails) {
   return (
-    <section className="flex items-center px-4 sm:px-0">
-      <div className="flex flex-col w-full justify-center">
+    <section className="flex items-center px-4 sm:px-0 h-full">
+      <div className="flex flex-col w-full sm:w-auto justify-center">
         <ImageModal detailedImage={detailedImage}>
           <Image
             src={image}
             width={0}
             height={0}
             alt={name}
-            className="w-full h-auto max-w-full max-h-[50vh] sm:max-h-[70vh] object-contain mx-auto"
+            className="w-full h-auto max-w-full max-h-[50vh] sm:max-h-[70vh] object-contain mx-auto sm:mx-0"
             id="active-image"
           />
         </ImageModal>
