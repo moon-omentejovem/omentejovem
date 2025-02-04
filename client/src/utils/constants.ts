d4c7af7d2de9640b2c9f8618f5b4a7662063f47f
yungwknd <yungwkndllc@gmail.com>
2025-02-04 13:26:02 -0800

    rarible
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -226,5 +226,9 @@ export const OVERRIDE_EXTERNAL_LINKS: Record<
   '0x28a6f816eae721fea4ad34c000077b5fe525fc3c:1': {
     name: 'OpenSea',
     link: 'https://opensea.io/assets/ethereum/0x28a6f816eae721fea4ad34c000077b5fe525fc3c/1'
+  },
+  '0x60f80121c31a0d46b5279700f9df786054aa5ee5:19434': {
+    name: 'Rarible',
+    link: 'https://rarible.com/token/0x60f80121c31a0d46b5279700f9df786054aa5ee5:19434'
   }
 }
