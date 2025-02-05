f204b6d1e2317b9ab95c9e16bcc0458999cb4294
Brujao <brunoffontebasso@gmail.com>
2025-02-05 19:28:07 -0300

    Fix: Owners modal alignment and responsiveness

--- client/src/components/Modals/OwnersModal.tsx
+++ client/src/components/Modals/OwnersModal.tsx
@@ -28,7 +28,7 @@ export function OwnersModal({
       <Dialog.Portal>
         <Dialog.Overlay className="modal-overlay fixed inset-0 bg-black/[30%] backdrop-blur-sm z-20">
           <Dialog.Content
-            className="modal-content fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-xl border-[1px] border-black bg-background z-30"
+            className="modal-content fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[90vw] sm:w-full max-w-xl border-[1px] border-[#B1B1B1] bg-background z-30"
             onOpenAutoFocus={() => {
               if (!isAnimating) {
                 modalAnimations(setIsAnimating)
@@ -38,7 +38,7 @@ export function OwnersModal({
               setOpen(false)
             }}
           >
-            <header className="flex-shrink-0 flex items-center justify-between px-8 py-4 border-b-[1px] border-black bg-background">
+            <header className="flex-shrink-0 flex items-center justify-between px-8 py-4 border-b-[1px] border-[#B1B1B1] bg-background">
               <p className="underline font-heading">ALL OWNERS</p>
               <Dialog.Close
                 onClick={() => {
@@ -48,7 +48,7 @@ export function OwnersModal({
                 <Icons.X />
               </Dialog.Close>
             </header>
-            <div className="flex-1 overflow-y-auto max-h-[90vh] px-8 py-4">
+            <div className="flex-1 overflow-y-auto max-h-[50vh] px-8 py-4">
               <ul>
                 {owners.map((owner, index) => (
                   <li key={index} className="mb-2">
@@ -56,7 +56,7 @@ export function OwnersModal({
                       href={`https://etherscan.io/address/${owner.owner_address}`}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="text-primary-50 underline"
+                      className="text-primary-50 underline break-all"
                     >
                       {owner.owner_address}
                     </a>{' '}
