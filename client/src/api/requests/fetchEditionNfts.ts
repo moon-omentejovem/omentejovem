302b50164146c758b01b665e4f6cab2161d17b31
yungwknd <yungwkndllc@gmail.com>
2025-03-29 13:34:19 -0700

    fixes
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- client/src/api/requests/fetchEditionNfts.ts
+++ client/src/api/requests/fetchEditionNfts.ts
@@ -8,48 +8,47 @@ import fetch from 'node-fetch'
 export async function fetchEditionNfts() {
   let ALL_DATA: { nfts: NFT[] } = { nfts: [] }
 
-  const formattedQuery = ALL_NFTS.map((nft) => {
-    const prefix = nft.startsWith('KT') ? 'tezos.' : 'ethereum.'
-    const tokenAddress = nft.split(':')[0]
-    const tokenId = nft.split(':')[1]
-    return `${prefix}${tokenAddress}.${tokenId}`
-  }).filter((nft) => nft !== '')
-
-  // Batch into groups of 50
-  const batches = formattedQuery.reduce((acc, nft, index) => {
-    const batchIndex = Math.floor(index / 50)
-    acc[batchIndex] = acc[batchIndex] || []
-    acc[batchIndex].push(nft)
-    return acc
-  }, [] as string[][])
+  const formattedQuery = ALL_NFTS.filter((nft) => !nft.startsWith('KT')).map(
+    (nft) => {
+      const tokenAddress = nft.split(':')[0]
+      const tokenId = nft.split(':')[1]
+      return {
+        contractAddress: `${tokenAddress}`,
+        tokenId: tokenId
+      }
+    }
+  )
 
   // Fetch each batch
-  for (const batch of batches) {
-    const data = await fetch(`${api.baseURL}?nft_ids=${batch.join(',')}`, {
-      method: 'GET',
-      headers: {
-        'X-API-KEY': api.apiKey || '',
-        Accept: 'application/json'
-      }
+  const data = await fetch(`${api.baseURL}`, {
+    method: 'POST',
+    headers: {
+      'X-API-KEY': api.apiKey || '',
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      tokens: formattedQuery
     })
+  })
 
-    const jsonData = (await data.json()) as { nfts: NFT[] }
-    ALL_DATA.nfts = [...ALL_DATA.nfts, ...jsonData.nfts]
-  }
+  const jsonData = await data.json()
+  const DATA_MAPPED = jsonData as { nfts: NFT[] }
+
   // Order by created_date newest first
   ALL_DATA.nfts.sort((a, b) => {
     return (
-      new Date(b.created_date || '').getTime() -
-      new Date(a.created_date || '').getTime()
+      new Date(b.timeLastUpdated || '').getTime() -
+      new Date(a.timeLastUpdated || '').getTime()
     )
   })
 
   // Only return if contract.type === 'ERC721'
   ALL_DATA.nfts = ALL_DATA.nfts.filter((nft) => {
-    if (nft.contract_address.startsWith('KT')) {
-      return nft.token_count !== 1
+    if (nft.contract.address.startsWith('KT')) {
+      return nft.tokenType !== 'ERC1155'
     }
-    return nft.contract.type === 'ERC1155'
+    return nft.contract.tokenType === 'ERC721'
   })
 
   return ALL_DATA
--- client/src/api/requests/fetchOneOfOneNfts.ts
+++ client/src/api/requests/fetchOneOfOneNfts.ts
@@ -8,49 +8,46 @@ import { ALL_NFTS } from '@/utils/constants'
 export async function fetchOneOfOneNfts() {
   let ALL_DATA: { nfts: NFT[] } = { nfts: [] }
 
-  const formattedQuery = ALL_NFTS.map((nft) => {
-    const prefix = nft.startsWith('KT') ? 'tezos.' : 'ethereum.'
-    const tokenAddress = nft.split(':')[0]
-    const tokenId = nft.split(':')[1]
-    return `${prefix}${tokenAddress}.${tokenId}`
-  }).filter((nft) => nft !== '')
-
-  // Batch into groups of 50
-  const batches = formattedQuery.reduce((acc, nft, index) => {
-    const batchIndex = Math.floor(index / 50)
-    acc[batchIndex] = acc[batchIndex] || []
-    acc[batchIndex].push(nft)
-    return acc
-  }, [] as string[][])
-
-  // Fetch each batch
-  for (const batch of batches) {
-    const data = await fetch(`${api.baseURL}?nft_ids=${batch.join(',')}`, {
-      method: 'GET',
-      headers: {
-        'X-API-KEY': api.apiKey || '',
-        Accept: 'application/json'
+  const formattedQuery = ALL_NFTS.filter((nft) => !nft.startsWith('KT')).map(
+    (nft) => {
+      const tokenAddress = nft.split(':')[0]
+      const tokenId = nft.split(':')[1]
+      return {
+        contractAddress: `${tokenAddress}`,
+        tokenId: tokenId
       }
-    })
+    }
+  )
 
-    const jsonData = (await data.json()) as { nfts: NFT[] }
-    ALL_DATA.nfts = [...ALL_DATA.nfts, ...jsonData.nfts]
-  }
+  const data = await fetch(`${api.baseURL}`, {
+    method: 'POST',
+    headers: {
+      'X-API-KEY': api.apiKey || '',
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      tokens: formattedQuery
+    })
+  })
+
+  const jsonData = (await data.json()) as { nfts: NFT[] }
+  ALL_DATA.nfts = jsonData.nfts
 
   // Order by created_date newest first
   ALL_DATA.nfts.sort((a, b) => {
     return (
-      new Date(b.created_date || '').getTime() -
-      new Date(a.created_date || '').getTime()
+      new Date(b.timeLastUpdated || '').getTime() -
+      new Date(a.timeLastUpdated || '').getTime()
     )
   })
 
   // Only return if contract.type === 'ERC721'
   ALL_DATA.nfts = ALL_DATA.nfts.filter((nft) => {
-    if (nft.contract_address.startsWith('KT')) {
-      return nft.token_count === 1
+    if (nft.contract.address.startsWith('KT')) {
+      return nft.tokenType !== 'ERC1155'
     }
-    return nft.contract.type === 'ERC721' || nft.contract.type === 'erc721'
+    return nft.contract.tokenType === 'ERC721'
   })
 
   return ALL_DATA
